1ba0d2264d00681fd5c3f71dddebab2876f90328
6kq <65254157+6kq@users.noreply.github.com>
2022-01-30 01:56:16 +0100

    Added blank format

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+/* some variables
+//
+//
+//
+*/
+
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
 const port = 3000;
+
+/* create databse connection
+//
+//
+//
+*/
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -17,12 +29,24 @@ con.connect(function (err) {
     console.log("Connected!");
 });
 
+/* starts the server
+//
+//
+//
+*/
+
 app.use(bodyParser.json());
 
 app.listen(port, () => {
     //console log when server is running
     console.log(`App Listening on port ${port}`);
 });
+
+/* as json format 
+/
+/
+/
+*/
 
 app.get("/info", (req, res) => {
     //get number of rows in table
@@ -59,6 +83,47 @@ app.post("/testdb", (req, res) => {
         return res.status(500).send("error with database");
     });
 });
+
+/* Blank request just output the value not in json format 
+//
+//
+//
+*/
+app.post("/testdb/blank", (req, res) => {
+    var givenHash = req.body.hash;
+    if (String(Object.keys(givenHash).length) != 128) return res.status(400).send("error invalid hash provided")
+    con.query('SELECT * FROM acchash WHERE hash', function (error, results, fields) {
+        if (results.length > 0) {
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].hash == givenHash) {
+                    return res.status(200).send("true"); //{"exists": true} as json
+                }
+            }
+            insertValur(givenHash);
+            return res.status(200).send("false"); //{"exists": false} as json
+        }
+        return res.status(500).send("error with database");
+    });
+});
+
+app.get("/info/blank", (req, res) => {
+    //get number of rows in table
+    con.query('SELECT * FROM acchash WHERE hash', function (error, results, fields) {
+        if (error) {
+            //returns error
+            res.status(500).send("error");
+            throw error;
+        }
+        //returns success + number of rows in table
+        res.status(200).send(results.length);
+    });
+});
+
+/* databse functions
+//
+//
+//
+*/
 
 function insertValur(hash) {
     con.query('INSERT INTO acchash (hash) VALUES (?)', [hash], function (error, results, fields) {
